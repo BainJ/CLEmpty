777d5925f7dd327e62cfa74044184df02d8e29a4
Bain Jim <devbainj@gmail.com>
2019-12-12 09:12:47 +0800

    first

---
+++ Classes/NJViewController.h
@@ -0,0 +1,13 @@
+//
+//  NJViewController.h
+//  NJVendor
+//
+//  Created by bainj on 04/18/2018.
+//  Copyright (c) 2018 bainj. All rights reserved.
+//
+
+@import UIKit;
+
+@interface NJViewController : UIViewController
+
+@end
---
+++ Classes/NJViewController.m
@@ -0,0 +1,29 @@
+//
+//  NJViewController.m
+//  NJVendor
+//
+//  Created by bainj on 04/18/2018.
+//  Copyright (c) 2018 bainj. All rights reserved.
+//
+
+#import "NJViewController.h"
+
+@interface NJViewController ()
+
+@end
+
+@implementation NJViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
